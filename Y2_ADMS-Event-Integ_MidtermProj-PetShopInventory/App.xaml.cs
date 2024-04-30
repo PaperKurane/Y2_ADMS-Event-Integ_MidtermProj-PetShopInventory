2c8bc4d2c023f1b88a7b960e9f61029ac262f007
PaperKurane <omginlol@gmail.com>
2024-04-30 10:21:37 +0800

    Add project files.

---
+++ Y2_ADMS-Event-Integ_MidtermProj-PetShopInventory/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Y2_ADMS_Event_Integ_MidtermProj_PetShopInventory
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
