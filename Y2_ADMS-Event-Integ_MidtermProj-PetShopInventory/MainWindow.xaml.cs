1da330bb2a185f5e41239d337076009807077a1b
PaperKurane <omginlol@gmail.com>
2024-04-30 23:59:25 +0800

    detail recognition system should be running now

--- Y2_ADMS-Event-Integ_MidtermProj-PetShopInventory/MainWindow.xaml.cs
+++ Y2_ADMS-Event-Integ_MidtermProj-PetShopInventory/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,60 @@ namespace Y2_ADMS_Event_Integ_MidtermProj_PetShopInventory
     /// </summary>
     public partial class MainWindow : Window
     {
+        PetChestConnDataContext _dbConn = null;
+        bool flag = false;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _dbConn = new PetChestConnDataContext(Properties.Settings.Default.PetCenterConnectionString);
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbUsername.Text.Length > 0 && tbPassword.Text.Length > 0)
+            {
+                IQueryable<Employee> selectResults = from s in _dbConn.Employees
+                                                     where s.Employee_ID == tbUsername.Text
+                                                     select s;
+
+                if (selectResults.Count() == 1)
+                {
+                    //MessageBox.Show("Username exists");
+
+                    foreach (Employee s in selectResults)
+                    {
+                        if (s.Employee_Password == tbPassword.Text)
+                        {
+                            string messageString = $"Login complete.";
+                            //if (s. == null)
+                            //    messageString += $" Welcome {s.LoginName}!";
+                            //else
+                            //    messageString += $" Welcome back {s.LoginName}! Havent seen you since {s.LoginDate}";
+
+                            MessageBox.Show(messageString);
+                            //s.LoginDate = DateTime.Now;
+
+                            Employee employ = new Employee();
+                            //employ.LoginID = s.LoginID;
+                            //employ.LogDate = (DateTime)s.LoginDate;
+
+                            //_dbConn.tblLogs.InsertOnSubmit(tlog);
+                            flag = true;
+                            break;
+                        }
+                    }
+                    //_dbConn.SubmitChanges();
+                }
+            }
+
+            if (flag)
+            {
+                // logic for logging in if flag is true
+                MessageBox.Show("You should now be logged in");
+            }
         }
     }
 }
--- Y2_ADMS-Event-Integ_MidtermProj-PetShopInventory/Properties/Settings.Designer.cs
+++ Y2_ADMS-Event-Integ_MidtermProj-PetShopInventory/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Y2_ADMS_Event_Integ_MidtermProj_PetShopInventory.Properties
-{
-
-
+namespace Y2_ADMS_Event_Integ_MidtermProj_PetShopInventory.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-4K3JKIQ\\SQLEXPRESS01;Initial Catalog=PetCenter;Integrated Sec" +
+            "urity=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False")]
+        public string PetCenterConnectionString {
+            get {
+                return ((string)(this["PetCenterConnectionString"]));
             }
         }
     }
